aab09d91dbfa91b1bd950643480bcce802e5002f
Jan Arnel R. Banaag <jrbanaag@findme.com.ph>
2025-08-20 16:16:49 +0800

    connect to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
+app.get("/products", (req, res) => {
+    res.send("Server is ready");
+});
+
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 });
